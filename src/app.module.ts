1eb2ab14148eae0f30e16135d0a5462a3e72d425
Park Dahyeon <dahyeon1712@dgu.ac.kr>
2022-04-28 02:05:21 +0900

    feat : MailerModule 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestModule } from './test/test.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import * as path from 'path'
 
 @Module({
-  imports: [],
+  imports: [TestModule, 
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    MailerModule.forRootAsync({
+			useFactory: (): object => {
+				return {
+					transport: process.env.EMAIL_ADMIN_AUTH,
+					defaults: {
+						from: process.env.EMAIL_ADMIN_SENDERINFO,
+					},
+					template: {
+						dir: path.join(__dirname, '/templates/'),
+						adapter: new EjsAdapter(),
+						options: {
+							strict: true,
+						},
+					},
+				};
+			},
+		}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
